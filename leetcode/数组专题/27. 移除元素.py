f34a336b66851eec2a291d64858d22e80a7b4052
Saitoler <1402145603@qq.com>
2021-10-17 15:38:09 +0800

    为 27题新增双指针解法

--- leetcode/数组专题/27. 移除元素.py
+++ leetcode/数组专题/27. 移除元素.py
@@ -24,6 +24,24 @@ def removeElement(nums, val):
     
     return count
 
+# 上面的解法只能说是钻题目漏洞，根本不算是解决了这道题
+# 这道题和26题类似，可以使用双指针的解法来进行解题
+# 定义双指针 slow, fast， 初始值都设置为0，
+# slow 指向的是不包含 val 的区间的末尾，fast 则在前面搜索 val, 如果是 val,就用后面的元素覆盖他
+
+def removeElement2(nums, val):
+    slow, fast = 0, 0
+
+    while fast < len(nums):
+        if nums[fast] != val:
+            nums[slow] = nums[fast]
+            slow += 1
+
+        fast += 1
+    return slow
+
+
+
 if __name__ == "__main__":
     print(removeElement([3,2,2,3], 3))
     print(removeElement([0,1,2,2,3,0,4,2], 2))
